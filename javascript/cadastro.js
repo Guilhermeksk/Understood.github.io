1588d6519871f6bf13049170c120e1de3098f9a0
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-23 13:34:43 -0300

    fazendo

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -1,3 +1,19 @@
+
+// chek de usuario
+const nameField = document.querySelector('input');
+
+nameField.addEventListener('input', () => {
+  nameField.setCustomValidity('');
+  nameField.checkValidity();
+    console.log(nameField.checkValidity());
+
+});
+
+nameField.addEventListener('invalid', () => {
+  nameField.setCustomValidity('Please fill in your First Name.');
+})
+
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
